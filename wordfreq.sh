889b42b9855c6ab5fcc10ba9b05379d3d96cf920
JortVisser <48459159+JortVisser@users.noreply.github.com>
2019-03-12 03:03:06 +0100

    Add files via upload
    Milestone 5 - word frequency list shell script

---
+++ wordfreq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Jort Visser
+# descr: generate a word frequency list for the word 'de'
+# 12-3-2018
+# Usage: ./wordfreq.sh FILE
+
+TEXT=$1
+if [ -z "$TEXT" ]
+then
+    echo "specify a file!"    				#copied from slides because it looked cool and the program would be extreme short without it.
+    exit
+fi
+
+echo "This program will print the amount of times the word de occures in the selected file."
+
+cat $TEXT | grep -i -o "\bde\b" | wc -l			#the word 'de' is surrounded by \b because otherwise it would also pick up occurences of 'de' in other words. 
